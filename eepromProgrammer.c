d3af5c0226a5aeb075ec1d6054a48337a448468e
Philip Hutchins <flipture@gmail.com>
2017-10-22 14:25:24 -0400

    Breaking apart methods experimenting with the clock and latch

--- eepromProgrammer.c
+++ eepromProgrammer.c
@@ -4,68 +4,18 @@
 #include <stdio.h>
 #include <string.h>
 
-// Header 11 - GPIO0
-// Header 13 - GPIO2
-// Header 15 - GPIO3
+// Header 11 - GPIO0 - SHIFT_CLK
+// Header 13 - GPIO2 - SHIFT_DATA
+// Header 15 - GPIO3 - SHIFT_LATCH
 
-// Should set these to the pins like RPI_V2_GPIO_P1_03...
-//#define SHIFT_DATA 13
-//#define SHIFT_CLK 11
-//#define SHIFT_LATCH 15
-#define SHIFT_DATA RPI_V2_GPIO_P1_13
 #define SHIFT_CLK RPI_V2_GPIO_P1_11
+#define SHIFT_DATA RPI_V2_GPIO_P1_13
 #define SHIFT_LATCH RPI_V2_GPIO_P1_15
 
 #define MAX 1000
 
-#define MODE_READ 0
-#define MODE_SET 1
-#define MODE_CLR 2
-#define MODE_INPUT_READ 3
-
-#define PULL_UP 0
-#define PULL_DOWN 1
-#define NO_PULL 2
-
-#define GPIO_BEGIN 0
-#define GPIO_END 1
-#define NO_ACTION 2
-
-#define NO_PIN 40 // Some big number that's beyond the connector's pin count
-#define DEBUG_OFF 0
-#define DEBUG_ON 1
-
-uint8_t  init = NO_ACTION;
-uint8_t  pull = NO_PULL;
-uint8_t  mode = MODE_READ;
-uint8_t  pin_number = NO_PIN;
-
-uint8_t i, len;
-uint8_t data, pin, debug_mode = DEBUG_OFF;
-
-void pushBits(char * bits) {
-  // Set mode for pin using fsel(pin, BCM2835_GPIO_FSEL_OUTP);
-  // Then set pin with bcm2835_gpio_set(pin);
-
-  long int i = 0;
-
-  while (bits[i]) {
-    switch (bits[i]) {
-      case '0':
-        printf("Bit is 0\n");
-        // Set data output pin to down
-      case '1':
-        printf("Bit is 1\n");
-        // Set data output pin to up
-    }
-    // Save data to register? (up, down up?, which pin?)
-
-    bcm2835_gpio_set_pud(pin, BCM2835_GPIO_PUD_UP);
-    bcm2835_gpio_set_pud(pin, BCM2835_GPIO_PUD_DOWN);
-    bcm2835_gpio_set_pud(pin, BCM2835_GPIO_PUD_UP);
-  }
-}
  
+// Convert HEX to Binary
 const char * hexToBin(char * hexString) {
   printf("Got hex string: %s\n", hexString);
 
@@ -73,8 +23,6 @@ const char * hexToBin(char * hexString) {
   char binstr[MAX];
   long int i = 0;
  
-  // printf("Enter the value for hexadecimal ");
-  // scanf("%s", hexa);
   while (hexString[i]) {
     switch (hexString[i]) {
       case '0':
@@ -130,18 +78,139 @@ const char * hexToBin(char * hexString) {
   return *binstr;
 }
 
-main() {
-  //#define SHIFT_DATA 13
-  //#define SHIFT_CLK 11
-  //#define SHIFT_LATCH 15
+void setPinUp(int pinNum) {
+  bcm2835_gpio_set_pud(pinNum, BCM2835_GPIO_PUD_UP);
 
+  return;
+}
+
+void setPinDown(int pinNum) {
+  bcm2835_gpio_set_pud(pinNum, BCM2835_GPIO_PUD_DOWN);
+
+  return;
+}
+
+void setPinOff(int pinNum) {
+  bcm2835_gpio_set_pud(pinNum, BCM2835_GPIO_PUD_OFF);
+
+  return;
+}
+
+void clearPin(int pinNum) {
+  bcm2835_gpio_fsel(pinNum, BCM2835_GPIO_FSEL_OUTP);
+  bcm2835_gpio_clr(pinNum);
+}
+
+void initPinSet(int pinNum) {
+  printf("Initializing pin number %i\n", pinNum);
+  sleep(1);
+  bcm2835_gpio_fsel(pinNum, BCM2835_GPIO_FSEL_OUTP);
+  //bcm2835_gpio_set_pud(pinNum, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set(pinNum);
+  //setPinOff(pinNum);
+  sleep(1);
+  clearPin(pinNum);
+
+  return;
+}
+
+// Clock pulse is up, down up or up, off, up ?
+void pulseClock() {
+  // Pulse the clock to shift the data one bit 
+  bcm2835_gpio_set_pud(SHIFT_CLK, BCM2835_GPIO_PUD_UP);
+  sleep(1);
+  clearPin(SHIFT_CLK);
+  //bcm2835_gpio_set_pud(SHIFT_CLK, BCM2835_GPIO_PUD_OFF);
+  sleep(1);
+  bcm2835_gpio_set_pud(SHIFT_CLK, BCM2835_GPIO_PUD_UP);
+  sleep(1);
+
+  return;
+}
+
+// Latch pulse is DOWN, UP, DOWN?
+void pulseLatch() {
+  // Pulse the latch
+  bcm2835_gpio_set_pud(SHIFT_LATCH, BCM2835_GPIO_PUD_DOWN);
+  sleep(1);
+  bcm2835_gpio_set_pud(SHIFT_LATCH, BCM2835_GPIO_PUD_UP);
+  sleep(1);
+  bcm2835_gpio_set_pud(SHIFT_LATCH, BCM2835_GPIO_PUD_DOWN);
+  sleep(1);
+
+  return;
+}
+
+// Load bits into the shift register
+void pushBits(char * bits) {
+  long int i = 0;
+  printf("Pushing bits %s\n", bits);
+
+  while (bits[i]) {
+    switch (bits[i]) {
+      case '0':
+        printf("Bit is 0\n");
+        setPinOff(SHIFT_DATA);
+      case '1':
+        printf("Bit is 1\n");
+        setPinUp(SHIFT_DATA);
+    }
+
+    sleep(1);
+
+    pulseClock();
+
+    i++;
+  }
+
+  return;
+}
+
+void gpio_reset(void) {
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_03, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_05, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_07, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_26, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_24, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_21, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_19, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_23, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_10, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_11, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_12, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_13, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_15, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_16, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_18, BCM2835_GPIO_PUD_OFF);
+  bcm2835_gpio_set_pud(RPI_V2_GPIO_P1_22, BCM2835_GPIO_PUD_OFF);
+
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_03, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_05, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_07, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_26, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_24, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_21, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_19, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_23, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_10, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_11, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_12, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_13, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_15, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_16, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_18, BCM2835_GPIO_FSEL_INPT);
+  bcm2835_gpio_fsel(RPI_V2_GPIO_P1_22, BCM2835_GPIO_FSEL_INPT);
+}
+
+main() {
   // Set each bit on SHIFT_DATA
   // Then pulse SHIFT_CLK
   // Repeat for each bit
   // When done...
   // Bring SHIFT_LATCH LOW then HIGH then LOW
 
-  char hexString[MAX] = "55";
+  // Use this to iterate through hex values 0-255
+  //char hexString[MAX] = "55";
   //char binString[MAX];
   //const char* binString = hexToBin(hexString);
   //printf("Binary string is: %s\n", binString);
@@ -149,41 +218,23 @@ main() {
 
   printf("Init BCM...\n");
 
+  // Init the BCM module
   if (!bcm2835_init()) return 1;
 
-  // GPIO begin here?? (look in gpio example)
+  // Set GPIO mode to set
+  initPinSet(SHIFT_DATA);
+  initPinSet(SHIFT_CLK);
+  initPinSet(SHIFT_LATCH);
 
-  GPIO_BEGIN;
+  pushBits("01010101");
+  //pushBits("11111111");
 
-  // Set GPIO mode
-  bcm2835_gpio_fsel(SHIFT_DATA, BCM2835_GPIO_FSEL_OUTP);
-  bcm2835_gpio_fsel(SHIFT_CLK, BCM2835_GPIO_FSEL_OUTP);
-  bcm2835_gpio_fsel(SHIFT_LATCH, BCM2835_GPIO_FSEL_OUTP);
+  pulseLatch();
 
-  // Set the data bit on the data pin
-  bcm2835_gpio_set_pud(SHIFT_DATA, BCM2835_GPIO_PUD_UP);
+  sleep(5);
 
-  // To clear (instead of down??)
-  //bcm2835_gpio_fsel(SHIFT_DATA, BCM2835_GPIO_FSEL_OUTP);
-  //bcm2835_gpio_clr(SHIFT_DATA);
-  //bcm2835_gpio_fsel(SHIFT_CLK, BCM2835_GPIO_FSEL_OUTP);
-  //bcm2835_gpio_clr(SHIFT_CLK);
-  //bcm2835_gpio_fsel(SHIFT_LATCH, BCM2835_GPIO_FSEL_OUTP);
-  //bcm2835_gpio_clr(SHIFT_LATCH);
-
-  // Pulse the clock to shift the data one bit 
-  bcm2835_gpio_set_pud(SHIFT_CLK, BCM2835_GPIO_PUD_UP);
-  bcm2835_gpio_set_pud(SHIFT_CLK, BCM2835_GPIO_PUD_DOWN);
-  //bcm2835_gpio_fsel(SHIFT_CLK, BCM2835_GPIO_FSEL_OUTP);
-  //bcm2835_gpio_clr(SHIFT_CLK);
-  bcm2835_gpio_set_pud(SHIFT_CLK, BCM2835_GPIO_PUD_UP);
-
-  // When done passing in data, pulse the latch
-  bcm2835_gpio_fsel(SHIFT_LATCH, BCM2835_GPIO_FSEL_OUTP);
-  bcm2835_gpio_clr(SHIFT_LATCH);
-  bcm2835_gpio_set_pud(SHIFT_LATCH, BCM2835_GPIO_PUD_UP);
-  bcm2835_gpio_fsel(SHIFT_LATCH, BCM2835_GPIO_FSEL_OUTP);
-  bcm2835_gpio_clr(SHIFT_LATCH);
+  // Reset everything back to default
+  gpio_reset();
 
   bcm2835_close();
   printf("... done!\n");
